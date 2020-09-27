f726e1bd28e30f3628b591e65cfa7f25809f0e24
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-09-27 15:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,9 +24,12 @@ function add2(n) {
   // Feel free to move things around!
   const two = 2
 }
+
 function add2(n){
+  var two=2
   return n+two;
 }
+
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
